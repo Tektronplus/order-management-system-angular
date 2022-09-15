2d2383e7fea48b3687972264f6518040443426a9
Davor Roman <davoroman01@gmail.com>
2022-09-15 10:26:43 +0200

    Initial components
    Added some fundamentals components & styling the main components, such as, header, main and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormOrderComponent } from './main/form-order/form-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    FormOrderComponent
   ],
   imports: [
     BrowserModule
