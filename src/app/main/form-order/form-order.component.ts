a87d3543122a70ebba4672c52a9a236eab2a881e
Davor Roman <davoroman01@gmail.com>
2022-09-15 14:48:18 +0200

    Form Orders
    I made the second part of order form

--- src/app/main/form-order/form-order.component.ts
+++ src/app/main/form-order/form-order.component.ts
@@ -3,23 +3,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form-order',
   templateUrl: './form-order.component.html',
-  styleUrls: ['./form-order.component.css']
+  styleUrls: ['./form-order.component.css'],
 })
 export class FormOrderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  
+  ngOnInit(): void {}
 
   /* VARIABLES */
   isTakeAway: boolean = false;
 
+  /* SETTERS & GETTERS  */
+  setIsTakeAway = () => (this.isTakeAway = !this.isTakeAway);
 
-  setIsTakeAway = () => this.isTakeAway = !this.isTakeAway;
+  /* METHODS */
+  addOrderForm() {
+    this.ordersFormDB.push(['Margherita', 1, 1]); //TEST
+  }
+
+  deleteOrderForm(index: number) {
+    this.ordersFormDB.splice(index, 1);
+  }
 
   /* PLACEHOLDER DATABASE */
-  tablesDB: string[] = ["n°01", "n°02", "n°03", "n°04", "n°05", "n°06", "n°07", "n°08", "n°09", "n°10", "n°11", "n°12", "n°13", "n°14", "n°15", "n°16", "n°17", "n°18", "n°19", "n°20"]
-  pizzaDB: Array<[string, number]> = [["Marinara", 4.90], ["Pugliese", 5.90], ["Napoli", 5.90], ["Contadino", 6.90], ["Capricciosa", 4.90], ["Bufala", 4.90], ["Monzese", 4.90], ["Ananas", 10.90]]
+  tablesDB: string[] = [
+    'n°01',
+    'n°02',
+    'n°03',
+    'n°04',
+    'n°05',
+    'n°06',
+    'n°07',
+    'n°08',
+    'n°09',
+    'n°10',
+    'n°11',
+    'n°12',
+    'n°13',
+    'n°14',
+    'n°15',
+    'n°16',
+    'n°17',
+    'n°18',
+    'n°19',
+    'n°20',
+  ];
+  pizzasDB: Array<[string, number]> = [
+    ['Marinara', 4.9],
+    ['Pugliese', 5.9],
+    ['Napoli', 5.9],
+    ['Contadino', 6.9],
+    ['Capricciosa', 4.9],
+    ['Bufala', 4.9],
+    ['Monzese', 4.9],
+    ['Ananas', 10.9],
+  ];
+  ordersFormDB: Array<[string, number, number]> = [['Margherita', 1, 1]];
 }
